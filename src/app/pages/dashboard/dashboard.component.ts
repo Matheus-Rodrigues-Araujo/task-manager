a4e8ba78504a8d90b4135cd485d6df69182b3858
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-03-23 03:17:50 -0300

    Components header and timer configured

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from '../../components/header/header.component';
 import { AsideComponent } from '../../components/footer/aside.component';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { NgOptimizedImage } from '@angular/common';
+import { TimerComponent } from '../../components/dashboard/timer/timer.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [HeaderComponent,SidebarComponent, AsideComponent, NgOptimizedImage],
+  imports: [HeaderComponent,SidebarComponent, AsideComponent, NgOptimizedImage, TimerComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
