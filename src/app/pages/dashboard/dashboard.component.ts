ce9e06c8e2012804bae4cf7fb8e226baeba6cf67
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-03-24 18:53:56 -0300

    Dashboard.component with new semantic HTML | activities.component with style formatted

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -4,12 +4,12 @@ import { AsideComponent } from '../../components/footer/aside.component';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { NgOptimizedImage } from '@angular/common';
 import { TimerComponent } from '../../components/dashboard/timer/timer.component';
-import { GoalsComponent } from '../../components/dashboard/goals/goals.component';
+import { ActivitiesComponent } from '../../components/dashboard/activities/activities.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [HeaderComponent,SidebarComponent, GoalsComponent, AsideComponent, NgOptimizedImage, TimerComponent],
+  imports: [HeaderComponent,SidebarComponent, ActivitiesComponent, AsideComponent, NgOptimizedImage, TimerComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
