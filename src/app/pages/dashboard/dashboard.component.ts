9e24cd68955ad1e81ecf2c9a5a62cfa0223b5e63
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-03-24 16:39:11 -0300

    layout updated

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -4,11 +4,12 @@ import { AsideComponent } from '../../components/footer/aside.component';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { NgOptimizedImage } from '@angular/common';
 import { TimerComponent } from '../../components/dashboard/timer/timer.component';
+import { GoalsComponent } from '../../components/dashboard/goals/goals.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [HeaderComponent,SidebarComponent, AsideComponent, NgOptimizedImage, TimerComponent],
+  imports: [HeaderComponent,SidebarComponent, GoalsComponent, AsideComponent, NgOptimizedImage, TimerComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
