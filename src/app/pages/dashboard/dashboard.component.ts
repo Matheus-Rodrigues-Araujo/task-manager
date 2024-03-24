99b04bccde0ac01350914ae513a2b1d2ea2dabc8
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-03-24 19:49:40 -0300

    styles.scss updated | new-task-component has been configured

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -6,11 +6,12 @@ import { NgOptimizedImage } from '@angular/common';
 import { TimerComponent } from '../../components/dashboard/timer/timer.component';
 import { ActivitiesComponent } from '../../components/dashboard/activities/activities.component';
 import { MenuComponent } from '../../components/dashboard/menu/menu.component';
+import { NewTaskComponent } from '../../components/dashboard/new-task/new-task.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [HeaderComponent,SidebarComponent, ActivitiesComponent, MenuComponent, AsideComponent, NgOptimizedImage, TimerComponent],
+  imports: [HeaderComponent,SidebarComponent, ActivitiesComponent, MenuComponent, NewTaskComponent,AsideComponent, NgOptimizedImage, TimerComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
