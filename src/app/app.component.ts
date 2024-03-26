444d372ad47314d7641d110e1272c49175b9125b
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-03-26 12:44:33 -0300

    Forms Style updated: colors and font-sizes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
-// import { LoginComponent } from './components/forms/login/login.component';
-// import { RegisterComponent } from './components/forms/register/register.component';
+
 
 @Component({
   selector: 'app-root',
