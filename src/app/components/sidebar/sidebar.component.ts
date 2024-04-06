245feeda262627ca7639fab8b077841b0c695988
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-04-06 00:13:58 -0300

    SidebarComponent updated

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { DropdownComponent } from '../dropdown/dropdown.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [DropdownComponent],
+  imports: [DropdownComponent, NgIf],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
